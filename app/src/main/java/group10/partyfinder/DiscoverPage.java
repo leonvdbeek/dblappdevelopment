ee8db48cc9429a978d4b5e62c620aec65cf2fa2e
LyoubomirKatzarov <lyoubomir.katzarov@yahoo.com>
2018-03-19 13:27:31 +0100

    dirty fix layout

--- app/src/main/java/group10/partyfinder/DiscoverPage.java
+++ app/src/main/java/group10/partyfinder/DiscoverPage.java
@@ -28,7 +28,7 @@ public class DiscoverPage extends Fragment {
         btnTEST.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 2",Toast.LENGTH_SHORT).show();
             }
         });
 
