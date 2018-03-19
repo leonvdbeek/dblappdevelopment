17752a04f8c12c362f2d12add93b43f6bc775fa0
Roy <royvanleeuwen@live.nl>
2018-03-19 14:52:20 +0100

    added apiClinet for retrofit 2
    also added the dependencies for retrofit 2 and gson

---
+++ app/src/main/java/group10/partyfinder/ApiClient.java
@@ -0,0 +1,41 @@
+package group10.partyfinder;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by Roy on 12/03/2018.
+ */
+
+public interface RestApiHandler {
+
+    //url is lenin.pythonanywhere.com/parties
+
+    //ToDo remove the test endpoint
+    //test endpoint, not to be used in final product
+    @GET("/parties")
+    Call<ArrayList<Party>> getParties();
+
+    @GET("/organized/{user}")
+    Call<ArrayList<Party>> usersMyParties(@Path("user") String user);
+
+    @GET("/saved/{user}")
+    Call<ArrayList<Party>> usersSavedParties(@Path("user") String user);
+
+    @GET("/todayParty")
+    Call<ArrayList<Party>> todayParties();
+
+    @GET("/futureParty")
+    Call<ArrayList<Party>> futureParties();
+
+    @POST("/parties")
+    Call<Void> hostParty(@Body Party party);
+
+    @POST("https://jsonplaceholder.typicode.com/posts")
+    Call<Post> testPost(@Body Post post);
+}
