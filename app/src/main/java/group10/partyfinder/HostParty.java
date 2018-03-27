f5356ec229cfaddc56bf03d4f8bcd08a4d000ddf
Vermeulen <m.vermeulen@student.tue.nl>
2018-03-27 21:49:43 +0200

    Update createPartiy activity and gitignore
    It is now possible to create a party (date does not work in this version).

--- app/src/main/java/group10/partyfinder/HostParty.java
+++ app/src/main/java/group10/partyfinder/HostParty.java
@@ -1,13 +1,18 @@
 package group10.partyfinder;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.util.Date;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,8 +27,23 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HostParty extends AppCompatActivity {
 
-    //get the instance of our database
+    // Get the instance of our database
     private DBSnapshot DB = DBSnapshot.getInstance();
+
+    private View view;
+    private Party partyObject;
+    private EditText ETname;
+    private EditText ETdescription;
+    private EditText ETstartDate;
+    private EditText ETstartTime;
+    private String startTime;
+    private EditText ETendDate;
+    private EditText ETendTime;
+    private String endTime;
+    private EditText ETtheme;
+    private EditText ETaddress;
+    private EditText ETlongitude;
+    private EditText ETlattitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +59,42 @@ public class HostParty extends AppCompatActivity {
 
         // Set title toolbar
         setTitle("Create party");
+
+        // Make variable for snackbar
+        view = findViewById(R.id.mainLayout);
+
+        ETname = findViewById(R.id.ETname);
+        ETdescription = findViewById(R.id.ETdescription);
+        ETstartDate = findViewById(R.id.ETstartDate);
+        ETstartTime = findViewById(R.id.ETstartTime);
+        ETendDate = findViewById(R.id.ETendDate);
+        ETendTime = findViewById(R.id.ETendTime);
+        ETtheme = findViewById(R.id.ETtheme);
+        ETaddress = findViewById(R.id.ETaddress);
+        ETlongitude = findViewById(R.id.ETlongitude);
+        ETlattitude = findViewById(R.id.ETlattitude);
+
+
     }
+
+    public void createParty(View v) {
+        partyObject = new Party(
+                0,
+                ETname.getText().toString(),
+                ETdescription.getText().toString(),
+                new Date(),
+                ETtheme.getText().toString(),
+                "114987278191137298218",
+                ETaddress.getText().toString(),
+                ETlongitude.getText().toString(),
+                ETlattitude.getText().toString()
+        );
+        partyObject.printParty();
+        postParty(partyObject);
+        Snackbar.make(view, "The party is created!", Snackbar.LENGTH_LONG).show();
+    }
+
+
 
     // Called when go back arrow (in the left top) is pressed.
     // Go back to previous activity.
@@ -49,7 +104,7 @@ public class HostParty extends AppCompatActivity {
         return true;
     }
 
-    //method to post a party to the server
+    // Method to post a party to the server
     private void postParty(Party party){
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
@@ -86,7 +141,7 @@ public class HostParty extends AppCompatActivity {
         });
     }
 
-    //method to post a party to the server
+    // Method to post the edited party to the server
     private void editParty(Party party){
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
