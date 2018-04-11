9a04ed03d2384c7c0adafedd934b37c5fbb32a3a
Vermeulen <m.vermeulen@student.tue.nl>
2018-04-11 10:53:07 +0200

    Added date/time pickers to the editParty activity

--- app/src/main/java/group10/partyfinder/EditParty.java
+++ app/src/main/java/group10/partyfinder/EditParty.java
@@ -1,5 +1,7 @@
 package group10.partyfinder;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.location.Address;
@@ -13,14 +15,18 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
@@ -58,12 +64,16 @@ public class EditParty extends AppCompatActivity {
     private String saveStartTime;
     private String saveEndTime;
     private String[] ETSD;
-    private String[] ETED;
+    private String[] startDate;
+    private String[] endDate;
 
     String longitude;
     String latitude;
 
     private Context context = this;
+
+    Calendar myCalendar = Calendar.getInstance();
+    Calendar mcurrentTime = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,18 +117,127 @@ public class EditParty extends AppCompatActivity {
         ETdescription.setText(partyObject.getInfo(), TextView.BufferType.EDITABLE);
 
         startTime = partyObject.getPartyViewDate().split(" ");
-        // Split start date
+
+        // Split start date for date picker
         ETSD = startTime[0].split("-");
-        ETstartDate.setText(ETSD[2] + "-" + ETSD[1] + "-" + ETSD[0],
-                TextView.BufferType.EDITABLE);
-        ETstartTime.setText(startTime[1], TextView.BufferType.EDITABLE);
+
+        myCalendar.set(Calendar.YEAR, Integer.parseInt(ETSD[2]));
+        // -1 to get the correct month
+        myCalendar.set(Calendar.MONTH, Integer.parseInt(ETSD[1])-1);
+        myCalendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(ETSD[0]));
+
+        // Set start time for time picker
+        mcurrentTime.set(Calendar.HOUR_OF_DAY, Integer.parseInt(startTime[1].split(":")[0]));
+        mcurrentTime.set(Calendar.MINUTE, Integer.parseInt(startTime[1].split(":")[1]));
+
+        ETstartDate.setText(startTime[0]);
+        ETstartTime.setText(startTime[1]);
 
         endTime= partyObject.getPartyViewEndDate().split(" ");
-        // Split end date
-        ETED = endTime[0].split("-");
-        ETendDate.setText(ETED[2] + "-" + ETED[1] + "-" + ETED[0],
-                TextView.BufferType.EDITABLE);
-        ETendTime.setText(endTime[1], TextView.BufferType.EDITABLE);
+
+        ETendDate.setText(endTime[0]);
+        ETendTime.setText(endTime[1]);
+
+        // Datepicker for start date
+        DatePickerDialog.OnDateSetListener sDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                // Update
+                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                ETstartDate.setText(sdf.format(myCalendar.getTime()));
+            }
+
+        };
+
+        ETstartDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(EditParty.this,
+                        sDate,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        // Datepicker for end date
+        DatePickerDialog.OnDateSetListener eDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                // Update
+                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                ETendDate.setText(sdf.format(myCalendar.getTime()));
+            }
+
+        };
+
+        ETendDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(EditParty.this,
+                        eDate,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        ETstartTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(EditParty.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        mcurrentTime.set(Calendar.HOUR_OF_DAY, selectedHour);
+                        mcurrentTime.set(Calendar.MINUTE, selectedMinute);
+
+                        // Update
+                        ETstartTime.setText(String.format("%02d:%02d", selectedHour, selectedMinute));
+                    }
+                }, hour, minute, true);
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+
+            }
+        });
+
+        // Timpicker for end time
+        ETendTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(EditParty.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        mcurrentTime.set(Calendar.HOUR_OF_DAY, selectedHour);
+                        mcurrentTime.set(Calendar.MINUTE, selectedMinute);
+
+                        // Update
+                        ETendTime.setText(String.format("%02d:%02d", selectedHour, selectedMinute));
+                    }
+                }, hour, minute, true);
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+
+            }
+        });
     }
 
     public void startEditParty(View v) {
@@ -126,9 +245,12 @@ public class EditParty extends AppCompatActivity {
         //first check connectivity
         internetConnectionCheck();
 
-        saveStartTime = ETstartDate.getText().toString()
+        startDate = ETstartDate.getText().toString().split("-");
+        endDate = ETendDate.getText().toString().split("-");
+
+        saveStartTime = startDate[2] + "-" + startDate[1] + "-" + startDate[0]
                 + "T" + ETstartTime.getText().toString() + ":00+02:00";
-        saveEndTime = ETendDate.getText().toString()
+        saveEndTime = endDate[2] + "-" + endDate[1] + "-" + endDate[0]
                 + "T" + ETendTime.getText().toString() + ":00+02:00";
         //Snackbar.make(view, saveStartTime, Snackbar.LENGTH_LONG).show();
 
